d7ae2ed503b497aa8f7e235b564de7025e4f7f2e
AlexD <147753654+AlexDedkov@users.noreply.github.com>
2024-08-16 11:43:45 +0300

    Add files via upload

---
+++ precache-manifest.d95e1cea25eddaafe7efb0f4280c1314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5ce9ffc3070d77bcd2f95b6cee9ab7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb8a7985cf32dff1fe08",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "49d53756381de06665bb",
+    "url": "/static/js/2.ba7e1342.chunk.js"
+  },
+  {
+    "revision": "cb8a7985cf32dff1fe08",
+    "url": "/static/js/main.debed008.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
